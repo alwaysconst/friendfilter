65e62ea33ad6a17861d5b729487469fae0e2b72c
most-effect <kostyash1990@gmail.com>
2016-03-29 17:17:12 +1000

    Добавил JS, VK API, вывод списка друзей в DOM

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
-VK.init({
-    apiId: 5379727
-    });
-VK.Auth.login(function(response) {
-    if (response.session) {
-        console.log('всё ок!');
+new Promise(function(resolve) {
+    if (document.readyState === 'complete') {
+        resolve();
     } else {
-        alert('Не удалось авторизоваться');
+        window.onload = resolve;
     }
-}, 2);
+}).then(function() {
+    return new Promise(function(resolve, reject) {
+        VK.init({
+            apiId: 5379727
+        });
+
+        VK.Auth.login(function(response) {
+            if (response.session) {
+                resolve(response);
+            } else {
+                reject(new Error('Не удалось авторизоваться'));
+            }
+        }, 2);
+    });
+}).then(function() {
+    return new Promise(function(resolve, reject) {
+        VK.api('friends.get', {fields:'photo_50,first_name,last_name', order:'name'}, function(response) {
+            if (response.error) {
+                reject(new Error(response.error.error_msg));
+            } else {
+
+                var source = friendItemTemplateLeft.innerHTML,
+                    templateFn = Handlebars.compile(source),
+                    template = templateFn({list: response.response});
+
+                left.innerHTML = template;
+
+                resolve();
+            }
+        });
+    });
+}).catch(function(e) {
+    alert('Ошибка: ' + e.message);
+});
